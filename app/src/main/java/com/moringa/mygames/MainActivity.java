4cb23ab48fce9d5dff3c72a49e9edaf3c406e44d
dutdavid <dutdavid7@gmail.com>
2019-03-03 11:31:13 +0300

    Games activity

--- app/src/main/java/com/moringa/mygames/MainActivity.java
+++ app/src/main/java/com/moringa/mygames/MainActivity.java
@@ -1,5 +1,7 @@
 package com.moringa.mygames;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
         mSearchGamesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Battle field 4", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, GamesActivity.class);
+                startActivity(intent);
             }
         });
     }

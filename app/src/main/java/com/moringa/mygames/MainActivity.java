4a408d12cf9a3576daaea3048ceb29e0578503de
dutdavid <dutdavid7@gmail.com>
2019-03-03 11:17:02 +0300

    battlefield 4 toast

--- app/src/main/java/com/moringa/mygames/MainActivity.java
+++ app/src/main/java/com/moringa/mygames/MainActivity.java
@@ -2,12 +2,24 @@ package com.moringa.mygames;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mSearchGamesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSearchGamesButton = (Button) findViewById(R.id.searchGamesButton);
+        mSearchGamesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Battle field 4", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }

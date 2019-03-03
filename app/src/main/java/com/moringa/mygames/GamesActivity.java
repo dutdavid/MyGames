efcc9c83f2bca19b677bfeb87e975d8be4fee37b
dutdavid <dutdavid7@gmail.com>
2019-03-03 11:54:55 +0300

    list view

--- app/src/main/java/com/moringa/mygames/GamesActivity.java
+++ app/src/main/java/com/moringa/mygames/GamesActivity.java
@@ -1,13 +1,30 @@
 package com.moringa.mygames;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class GamesActivity extends AppCompatActivity {
+    private TextView mLocationTextView;
+    private ListView mListView;
+    private String[] games = new String[] {"Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("This are all the games in: " + location);
     }
 }
--- app/src/main/java/com/moringa/mygames/MainActivity.java
+++ app/src/main/java/com/moringa/mygames/MainActivity.java
@@ -4,23 +4,29 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mSearchGamesButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mSearchGamesButton = (Button) findViewById(R.id.searchGamesButton);
+
         mSearchGamesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this, GamesActivity.class);
+                intent.putExtra("location", location);
                 startActivity(intent);
             }
         });

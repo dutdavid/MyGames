1bf7f3bf592992495eeaeadb5eb53673a3ad4e7a
dutdavid <dutdavid7@gmail.com>
2019-03-03 12:00:29 +0300

    games

--- app/src/main/java/com/moringa/mygames/GamesActivity.java
+++ app/src/main/java/com/moringa/mygames/GamesActivity.java
@@ -9,11 +9,12 @@ import android.widget.TextView;
 public class GamesActivity extends AppCompatActivity {
     private TextView mLocationTextView;
     private ListView mListView;
-    private String[] games = new String[] {"Mi Mero Mole", "Mother's Bistro",
-            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
-            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
-            "Lardo", "Portland City Grill", "Fat Head's Brewery",
-            "Chipotle", "Subway"};
+    private String[] games = new String[] {"Mortal kombat 11", "FIFA 19",
+            "Far Cry New ", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Far Cry New Dawn", "Ghost Recon", "NBA 2k19", "Just Cause $",
+            "Jump Force", "Battle Field 5", "Yakuza 0", "Initial Stage 2",
+            "Lords of The Fallen", "Project Cars 2", "Devil May Cry 5",
+            "Call of Duty world war 2", "Sniper elite 4"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
